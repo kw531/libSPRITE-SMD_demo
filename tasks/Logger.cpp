7d569f08e4dc2786f04eced99e474d063b393c57
Daniel Heater <daniel@heatertech.com>
2014-07-10 11:01:10 -0500

    Make sure we flush the logger queues on terminate

--- tasks/Logger.cpp
+++ tasks/Logger.cpp
@@ -7,6 +7,28 @@
 namespace task
 {
 
+    static bool log_stuff(FILE* fp)
+    {
+        static queues::SMD_queue& smd_q = queues::SMD_queue::get_instance();
+        static telem::SMD_msg_t smd_msg;
+
+        while(false == smd_q.is_empty())
+        {
+            if(false == smd_q.read(smd_msg))
+            {
+                EPRINTF("Error reading data from SMD queue\n");
+            }
+            fprintf(fp, "%g,%g,%g,%g\n",
+                    double(smd_msg.time),
+                    double(smd_msg.position),
+                    double(smd_msg.velocity),
+                    double(smd_msg.acceleration));
+        }
+
+        return true;
+    }
+
+
     Logger::Logger(const char* const name, const char* const logfile) :
         SRTX::Task(name),
         m_filename(logfile),
@@ -33,28 +55,16 @@ namespace task
 
     bool Logger::execute()
     {
-        static queues::SMD_queue& smd_q = queues::SMD_queue::get_instance();
-        static telem::SMD_msg_t smd_msg;
-
-        while(false == smd_q.is_empty())
-        {
-            if(false == smd_q.read(smd_msg))
-            {
-                EPRINTF("Error reading data from SMD queue\n");
-            }
-            fprintf(m_ofp, "%g,%g,%g,%g\n",
-                    double(smd_msg.time),
-                    double(smd_msg.position),
-                    double(smd_msg.velocity),
-                    double(smd_msg.acceleration));
-        }
-
-        return true;
+        return log_stuff(m_ofp);
     }
 
 
     void Logger::terminate()
     {
+        /* Pull any remaining stuff from the log queues and write it before
+         * closing the log file.
+         */
+        log_stuff(m_ofp);
         fclose(m_ofp);
     }
 
