00ae3ce4933c529d564cd657c3ba0edf64eb39cd
Daniel Heater <daniel@heatertech.com>
2014-08-05 13:02:12 -0500

    Update smd.lua to use the newer scheduler.lua file provided by libSPRITE

--- smd.lua
+++ smd.lua
@@ -1,4 +1,5 @@
-require 'scheduler'
+package.path = '/usr/local/lib/SPRITE/?.lua;' .. package.path
+s = require 'scheduler'
 
 --------------------------------------------------------------------------------
 -- Set some constants.
@@ -19,17 +20,18 @@ tp = Task_properties.new()
 priority = tp:MAX_USER_TASK_PRIO()
 
 -- Create the scheduler.
-s = create_scheduler(tp, priority)
+SCHEDULER_PERIOD = s.HZ_to_period(10)
+scheduler = s.create(tp, SCHEDULER_PERIOD, priority)
 priority = priority - 1
 
 -- Create task that manages the SMD serial port.
 smd = SMD.new("SMD", k, B, mass)
-set_task_properties(smd, tp, FASTEST_PERIOD, priority)
+s.set_task_properties(smd, tp, SCHEDULER_PERIOD, priority)
 priority = priority - 1
 
 -- Create a data logger task.
 logger = Logger.new("Logger", LOGFILE)
-set_task_properties(logger, tp, HZ_to_period(1), priority)
+s.set_task_properties(logger, tp, s.HZ_to_period(1), priority)
 priority = priority - 1
 
 -- Start everything up.
@@ -44,6 +46,10 @@ debug:debug()
 
 -- Halt the task
 smd:stop()
+
+-- Let the logger run until it has had time to flush all data.
+os.execute("sleep " .. tonumber(2))
 logger:stop()
+scheduler:stop()
 
 print "...Exiting"
