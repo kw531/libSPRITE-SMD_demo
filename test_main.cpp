910a326587209cea2fdb3ea32869b84348dda947
Daniel Heater <daniel@heatertech.com>
2014-08-29 11:42:58 -0500

    I realized now that allowing the task to run as fast as possible causes problems on multi-core. Put some delays in as a workaround.

--- test_main.cpp
+++ test_main.cpp
@@ -1,12 +1,15 @@
 #include "artemis_if.h"
+#include <SRTX/RTC.h>
 
 int main(int arc, char* argv[])
 {
     fsw_init(argv[1]);
 
+    SRTX::sleep(units::Nanoseconds(10 * units::MSEC));
     for(unsigned int i = 0; i < 1000; ++i)
     {
         fsw_execute();
+        SRTX::sleep(units::Nanoseconds(5 * units::MSEC));
     }
 
     fsw_terminate();
